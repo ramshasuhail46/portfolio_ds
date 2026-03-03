802940b90d730a71da344ac9a73dab07ee5fadfe
ramshasuhail46 <ramshasuhail46@gmail.com>
2026-03-03 14:26:06 +0500

    Fix: workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  basePath: '/portfolio-ds',
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support the default Next.js Image Optimization
+  },
 };
 
 export default nextConfig;
